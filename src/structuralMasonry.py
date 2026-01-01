4da25992a20b255751c63c8a72962867c6681041
Paul Valentine <paul@the-valentines.co.uk>
2026-01-01 16:55:08 +0000

    Narrative added to the code

--- src/structuralMasonry.py
+++ src/structuralMasonry.py
@@ -1,19 +1,22 @@
+import sympy as sp
+
 class Masonry():
     def __init__(self,fb:float,fm:float,k:float)->None:
         """ define the main properties of the masonry """
-        self.fb = fb
-        self.fm = fm
-        self.k = k
+        self.fb: float = fb # normalised compressive strenght of the unit
+        self.fm: float = fm # comressive strength of the mortar
+        self.k: float = k # factor for compressive strength
         '''
         K = 0.75 typically for block Group 1 units in general purpose mortar
         K = 0.50 typically for clay brick group 2 units in general purpose mortar
         '''
-        self.a = 0.70  # alpha for standard mortar beds
-        self.b = 0.30  # beta factor
-        self.gm = 2.7  # partial factor on material strength
-        self.creep_coefficient = 1.5
+        self.a: float = 0.70  # alpha for standard mortar beds
+        self.b: float = 0.30  # beta factor
+        self.gm: float = 2.7  # partial factor on material strength
+        self.creep_coefficient: float = 1.5
 
         # calculate the characteristic compressive strength of the masonry
+
         self.fk = self.k * self.fb ** self.a * self.fm ** self.b
 
         # calculate the instant elastic modulus
